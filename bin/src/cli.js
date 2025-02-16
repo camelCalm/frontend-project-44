6728b231b0979cd3a651fd40934e86394a8b3264
camelCalm <sabirovich2014@mail.ru>
2025-02-16 16:04:55 +0300

    #3

---
+++ bin/src/cli.js
@@ -0,0 +1,7 @@
+import { question } from 'readline-sync';
+
+export default () => {
+    console.log('Welcome to the Brain Games!');
+    const name = question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
