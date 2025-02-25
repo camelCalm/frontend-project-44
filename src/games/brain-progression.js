9f0100e9c8a3838c387d943fb31c9a72b763e8e2
camelCalm <sabirovich2014@mail.ru>
2025-02-25 22:11:44 +0300

    #8

---
+++ src/games/brain-progression.js
@@ -0,0 +1,47 @@
+import randomNumber from '../randomNumber.js';
+import ranReasNum from '../randomReasonNumber.js';
+import index from '../index.js';
+
+export default (name) => {
+    console.log('What number is missing in the progression?');
+    let i = 0;
+
+    let iter = () => {
+        if (i === 3) {
+            console.log(`Congratulations, ${name}!`);
+            return;
+        }
+
+        let min = 5;
+        let max = 10;
+        let factor = 15;
+        let ranNum = randomNumber(factor)
+        let count = ranReasNum(min, max);
+        let res = [];
+        res.push(ranNum);
+
+        for (let i = 0; i < count; i += 1) {
+            res.push(res[res.length - 1] + ranNum);
+        }
+
+        let indexHideNum = ranReasNum(0, count);
+        let valueHideNum = res[indexHideNum];
+        res.splice(indexHideNum, 1, "' '");
+
+        let question = res.join(' ');
+        let answer = valueHideNum + '';
+
+        let answerOfIndex = index(question, answer);
+
+        if (answerOfIndex.result === true) {
+            console.log('Correct!');
+            i += 1;
+            return iter();
+        } else {
+            console.log(`'${answerOfIndex.answer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+            return;
+        }
+    };
+
+    return iter ();
+};
--- src/start.js
+++ src/start.js
@@ -2,12 +2,13 @@ import { question } from 'readline-sync';
 import even from './games/brain-even.js'
 import calc from './games/brain-calc.js';
 import gcd from './games/brain-gcd.js';
+import progression from './games/brain-progression.js'
 import cli from './cli.js';
 
 export default () => {
     const name = cli();
 
-    console.log('What is a game do you want to play? [even/calc/gcd/progression/prime]');
+    console.log('What is a game do you want to play? [even/calc/gcd/prg/prime]');
     const game = question(`Your answer: `);
 
     switch (game) {
@@ -17,6 +18,8 @@ export default () => {
             return calc(name);
         case 'gcd':
             return gcd(name);
+        case 'prg':
+            return progression(name);
         default:
             console.log('Missing from the list');
     }
